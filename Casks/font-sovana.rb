41fb7fe7cdf4a969aa73fb06f0bafb0695ca8308
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-03-19 19:04:30 -0700

    feat(Casks): add font-sovana.rb

---
+++ Casks/font-sovana.rb
@@ -0,0 +1,11 @@
+cask "font-sovana" do
+    version "1.0.0"
+    sha256 "3b03a37b22ad6d6ee47a87ae4e252b71e0fe0e06bac6f7ea8bc6940b1ebac2f6"
+
+    url "https://github.com/cyanghxst/homebrew-cyanghxst/releases/download/font-sovana-#{version}/Sovana.zip"
+    name "Sovana"
+    desc "Sovana regular font"
+    homepage "https://example.com"
+
+    font "Sovana-Regular.ttf"
+end
