ed14c597aadbfea852fffee203c52f79e04764bb
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-04-21 23:00:34 -0700

    style: format Ruby files with 2-space indentation

--- Casks/font-sovana.rb
+++ Casks/font-sovana.rb
@@ -1,11 +1,11 @@
 cask "font-sovana" do
-    version "1.0.0"
-    sha256 "3b03a37b22ad6d6ee47a87ae4e252b71e0fe0e06bac6f7ea8bc6940b1ebac2f6"
+  version "1.0.0"
+  sha256 "3b03a37b22ad6d6ee47a87ae4e252b71e0fe0e06bac6f7ea8bc6940b1ebac2f6"
 
-    url "https://github.com/cyanghxst/homebrew-cyanghxst/releases/download/font-sovana-#{version}/Sovana.zip"
-    name "Sovana"
-    desc "Sovana regular font"
-    homepage "https://www.behance.net/gallery/147291051/Sovana-Typeface"
+  url "https://github.com/cyanghxst/homebrew-cyanghxst/releases/download/font-sovana-#{version}/Sovana.zip"
+  name "Sovana"
+  desc "Sovana regular font"
+  homepage "https://www.behance.net/gallery/147291051/Sovana-Typeface"
 
-    font "Sovana-Regular.ttf"
+  font "Sovana-Regular.ttf"
 end
--- Casks/sideloadly.rb
+++ Casks/sideloadly.rb
@@ -1,11 +1,11 @@
 cask "sideloadly" do
-    version "0.60.0"
-    sha256 "428d062af1ca819712fb12cb0ace25fa49c80d9735c73cda3cbaf09ffcd63212"
+  version "0.60.0"
+  sha256 "428d062af1ca819712fb12cb0ace25fa49c80d9735c73cda3cbaf09ffcd63212"
 
-    url "https://sideloadly.io/SideloadlySetup.dmg"
-    name "Sideloadly"
-    desc "Sideload your favorite games and apps on iOS, Apple Silicon Macs, and Apple TV without jailbreak."
-    homepage "https://sideloadly.io"
+  url "https://sideloadly.io/SideloadlySetup.dmg"
+  name "Sideloadly"
+  desc "Sideload your favorite games and apps on iOS, Apple Silicon Macs, and Apple TV without jailbreak."
+  homepage "https://sideloadly.io"
 
-    app "Sideloadly.app"
+  app "Sideloadly.app"
 end
