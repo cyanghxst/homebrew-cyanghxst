efe1725c6a372196b06797e8f83be6b76953904f
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2026-04-24 02:31:23 -0700

    fix(Casks): add sha256 sum for arm64 and x64 to claude-prism.rb

--- Casks/claude-prism.rb
+++ Casks/claude-prism.rb
@@ -1,12 +1,9 @@
 cask "claude-prism" do
-  version "1.1.6"
+  arch arm: "arm64", intel: "x64"
 
-  on_arm do
-    sha256 "3dae0b581e3aa96aa48d7e6c2fed70d0a4c6dddb0b6035e4fa2f0742f4f2fbcf"
-  end
-  on_intel do
-    sha256 "cd942a395a83f7217b6b91c47ff219c2f1007749d0d03871603c6243a8e881f5"
-  end
+  version "1.1.6"
+  sha256 arm:   "3dae0b581e3aa96aa48d7e6c2fed70d0a4c6dddb0b6035e4fa2f0742f4f2fbcf",
+         intel: "cd942a395a83f7217b6b91c47ff219c2f1007749d0d03871603c6243a8e881f5"
 
   url "https://github.com/delibae/claude-prism/releases/download/v#{version}/ClaudePrism-macOS.dmg"
   name "ClaudePrism"
